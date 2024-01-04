eac2c0d625c165a737eb70afb9768e727f7919ef
Yosiris <yosiris0510@gmail.com>
2024-01-04 13:24:58 +0100

    chore: render html and show menu

---
+++ src/js/header.js
@@ -0,0 +1,29 @@
+function updateHeader() {
+  const open = document.getElementById('open');
+  const close = document.querySelector(".close");
+  const options = document.querySelector(".options-menu");
+
+  open?.addEventListener("click", openMenu);
+  close?.addEventListener("click", closeMenu);
+
+  function openMenu() {
+    open.style.display = "none";
+    close.style.display = "block";
+    options.style.display = "block";
+
+    if (options) {
+      options.style.top = "0px";
+    }
+  }
+
+  function closeMenu() {
+    open.style.display = "block";
+    close.style.display = "none";
+    options.style.display = "none";
+
+    if (options) {
+      options.style.top = "-100%";
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", updateHeader);
