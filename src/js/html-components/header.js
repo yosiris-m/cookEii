e9d52c6af3a7d49f78b367727dcb2caef100f26c
Yosiris <yosiris0510@gmail.com>
2024-01-09 14:55:43 +0100

    chore: add favicon and refactor styles

--- src/js/html-components/header.js
+++ src/js/html-components/header.js
@@ -1,60 +1,61 @@
 class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
         <header id="header" class="container-header">
-            <p class="header-title">CookEii</p>
-            <nav class="box-nav">
-                <menu id="open" class="open"><i class="fa-solid fa-bars"></i></menu>
-                <menu id="close" class="close"><i class="fa-solid fa-x"></i></menu>
-                <ul class="options-menu">
+        <img class="logo" src="/src/images/logo2.png" alt="logo" /> 
+          <nav class="box-nav">
+            <div class="botons"> 
+              <span id="open" class="open"><i class="fa-solid fa-bars"></i></span>
+              <span id="close" class="close"><i class="fa-solid fa-x"></i></span>
+            </div>
+              <ul class="options-menu">
                 <li>
                     <a class="link" href="login/login.html">Iniciar sesión</a>
-                </li>
                 <li>
                     <a class="link" href="login/createAcount.html">Crear cuenta</a>
                 </li>
                 <li><a class="link" href="#">Recetas favoritas</a></li>
                 <li><a class="link" href="#">Crear receta</a></li>
-                </ul>
+              </ul>
             </nav>
         </header>
       `;
-      
-      this.addEventsToHeader();
-    }
-    
-    addEventsToHeader() {
-        const open = document.getElementById('open');
-        const close = document.querySelector(".close");
-        const options = document.querySelector(".options-menu");
-      
-        open?.addEventListener("click", openMenu);
-        close?.addEventListener("click", closeMenu);
-      
-        function openMenu() {
-          open.style.display = "none";
-          close.style.display = "block";
-          options.style.display = "block";
-      
-          if (options) {
-            options.style.top = "0px";
-          }
-        }
-      
-        function closeMenu() {
-          open.style.display = "block";
-          close.style.display = "none";
-          options.style.display = "none";
-      
-          if (options) {
-            options.style.top = "-100%";
-          }
-        }
-      }
+
+    this.addEventsToHeader();
   }
-  
-  customElements.define('common-header', Header);
+
+  addEventsToHeader() {
+    const open = document.getElementById("open");
+    const close = document.querySelector(".close");
+    const options = document.querySelector(".options-menu");
+
+    open?.addEventListener("click", openMenu);
+    close?.addEventListener("click", closeMenu);
+
+    function openMenu() {
+      open.style.display = "none";
+      close.style.display = "block";
+      options.style.display = "block";
+
+      if (options) {
+        options.style.top = "0px";
+      }
+    }
+
+    function closeMenu() {
+      open.style.display = "block";
+      close.style.display = "none";
+      options.style.display = "none";
+
+      if (options) {
+        options.style.top = "-100%";
+      }
+    }
+  }
+}
+
+customElements.define("common-header", Header);
